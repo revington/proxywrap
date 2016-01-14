c039ad3d5fb5b613cd456caf98924ce07f69dd4f
Pedro Narciso García Revington <p.revington@gmail.com>
2016-01-14 09:29:31 +0100

    Updated test to cover strict and non-strict scenarios

--- test/issue-15.js
+++ test/issue-15.js
@@ -5,9 +5,7 @@ var http = require('http'),
     net = require('net'),
     exec = require('child_process').exec,
     child,
-    proxiedHttp = require('..').proxy(http, {
-        strict: false
-    });
+    proxyWrap = require('..');
 
 function findCloseWaitConnections(port, callback) {
     var child = exec('netstat -tonp | grep 8000 | grep CLOSE_WAIT',
@@ -19,35 +17,71 @@ function findCloseWaitConnections(port, callback) {
         });
 }
 
-describe('Sockets closed before any write #15', function () {
-    var port, server;
+function reproduce(proxyWrapConf, callback) {
+    var socket, server, port, proxiedHttp;
+    if (!callback) {
+        callback = proxyWrapConf;
+        proxyWrapConf = null;
+    }
 
-    before(function (done) {
-        var socket;
-        server = proxiedHttp.createServer(function handler(req, res) {
-            throw new Error('For this test socket should not call #write()');
-        }).listen(function (err) {
-            if (err) {
-                return done(err);
-            }
-            port = this.address().port;
-            socket = net.connect({
-                port: port
-            }, function () {
-                socket.end();
+    proxiedHttp = proxyWrap.proxy(http, proxyWrapConf);
+
+    server = proxiedHttp.createServer(function handler(req, res) {
+        throw new Error('For this test socket should not call #write()');
+    }).listen(function (err) {
+        if (err) {
+            return done(err);
+        }
+        port = this.address().port;
+        socket = net.connect({
+            port: port
+        }, function () {
+            socket.end();
+        });
+        socket.on('end', function () {
+            return callback(null, server);
+        });
+    });
+}
+describe('Sockets closed before any write #15', function () {
+    describe('On strict mode', function () {
+        var port, server;
+        before(function (done) {
+            reproduce(function (err, _server) {
+                server = _server;
+                port = server.address().port;
+                return done();
             });
-            socket.on('end', function () {
-                done();
+        });
+        after(function () {
+            server.close();
+        });
+        it('should be restored', function (done) {
+            findCloseWaitConnections(port, function (err, stdout) {
+                assert.deepEqual(null, stdout);
+                return done();
             });
         });
     });
-    after(function () {
-        server.close();
-    });
-    it('should be restored', function (done) {
-        findCloseWaitConnections(port, function (err, stdout) {
-            assert.deepEqual(null, stdout);
-            return done();
+    describe('On non-strict mode', function () {
+        var port, server;
+        before(function (done) {
+            reproduce({
+                strict: false
+            }, function (err, _server) {
+                server = _server;
+                port = server.address().port;
+                return done();
+            });
+        });
+        after(function () {
+            server.close();
+        });
+        it('should be restored', function (done) {
+            findCloseWaitConnections(port, function (err, stdout) {
+                assert.deepEqual(null, stdout);
+                return done();
+            });
         });
     });
 });
